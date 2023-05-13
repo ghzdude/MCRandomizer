c2dadd620de5200d844398b2a810baad71164076
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-05-13 16:29:39 -0700

    itemrandomizer description

--- src/main/java/com/ghzdude/randomizer/ItemRandomizer.java
+++ src/main/java/com/ghzdude/randomizer/ItemRandomizer.java
@@ -1,4 +1,12 @@
 package com.ghzdude.randomizer;
 
+/* Item Randomizer
+ * Goal is to give the player a random item every so often
+ * Every so often, points are added to a counter
+ * those points are then used to give the player an item
+ * more points give bigger stacksize of item at once
+ * items have a defined value, otherwise stacksize is used
+ */
+
 public class ItemRandomizer {
 }
