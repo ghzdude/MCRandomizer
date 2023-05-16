5f0b94627b837b895147626be357eedb10b3d97b
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-05-15 23:44:38 -0700

    start documentation on StructureRandomizer

---
+++ src/main/java/com/ghzdude/randomizer/StructureRandomizer.java
@@ -0,0 +1,7 @@
+package com.ghzdude.randomizer;
+
+/* Structure Randomizer description
+ * every so often, generate a structure at some random x, z coordinate near the player
+ */
+public class StructureRandomizer {
+}
