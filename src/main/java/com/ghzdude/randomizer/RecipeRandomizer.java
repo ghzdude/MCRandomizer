ad5844bf1e3dd52befb02639776a6ad2600961ad
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-05-18 20:06:31 -0700

    begin implement RecipeRandomizer comment out RecipeRandomizer register

--- src/main/java/com/ghzdude/randomizer/RecipeRandomizer.java
+++ src/main/java/com/ghzdude/randomizer/RecipeRandomizer.java
@@ -1,9 +1,26 @@
 package com.ghzdude.randomizer;
 
+import net.minecraft.data.recipes.*;
+import net.minecraft.world.item.crafting.*;
+
 /* Recipe Randomizer Description
  * on resource re/load, randomize every recipe
  */
-
 public class RecipeRandomizer {
+    public void randomizeRecipe(Recipe<?> recipe) {
+        ItemRandomizer randomizer = new ItemRandomizer();
 
+        SpecialRecipeBuilder specialBuilder;
+        ShapedRecipeBuilder shapedBuilder;
+        ShapelessRecipeBuilder shapelessBuilder;
+
+        if (recipe instanceof ShapedRecipe){
+            shapedBuilder = new ShapedRecipeBuilder(randomizer.getRandomItem().item, recipe.getResultItem().getCount());
+            shapedBuilder.pattern(recipe.getGroup());
+        }
+
+        if (recipe instanceof ShapelessRecipe) {
+            shapelessBuilder = new ShapelessRecipeBuilder(randomizer.getRandomItem().item, recipe.getResultItem().getCount());
+        }
+    }
 }
