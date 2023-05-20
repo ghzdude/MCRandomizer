e3daeae9f1b6fbf687d8c4d35e987c89af04d8f4
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-05-20 02:14:42 -0700

    add more configs

--- src/main/java/com/ghzdude/randomizer/RandomizerConfig.java
+++ src/main/java/com/ghzdude/randomizer/RandomizerConfig.java
@@ -1,32 +1,39 @@
 package com.ghzdude.randomizer;
 
-import net.minecraft.core.NonNullList;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
 import net.minecraftforge.common.ForgeConfigSpec;
 import org.apache.commons.lang3.tuple.Pair;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.function.Predicate;
 
 public class RandomizerConfig {
 
     public static final Pair<RandomizerConfig, ForgeConfigSpec> RandomizerConfigPair;
     public final ForgeConfigSpec.ConfigValue<Integer> itemCooldown;
-    // public final ForgeConfigSpec.ConfigValue<List<? extends Item>> blacklistedItems;
+    public final ForgeConfigSpec.ConfigValue<Boolean> randomizeRecipes;
+    public final ForgeConfigSpec.ConfigValue<Boolean> giveRandomItems;
+    public final ForgeConfigSpec.ConfigValue<Boolean> generateStructures;
+
 
     RandomizerConfig (ForgeConfigSpec.Builder builder) {
 
-        itemCooldown = builder.comment("time between items given (measured in ticks, 20 ticks is one second)")
-                .define("item_cooldown", 1200);
+        // Item Randomizer
+        builder.push("Item Randomizer");
+        this.giveRandomItems = builder.comment("Should random items be given to the player? Defaults to true")
+                .define("give_random_items", true);
 
-        // shit don't work
-        /*blacklistedItems = builder.comment("List of Items/Blocks that shouldn't be given to the player")
-                .defineList("item_blacklist", new BlacklistedItems().get(), o -> o instanceof Item);
-        */
+        this.itemCooldown = builder.comment("Time between items given (measured in ticks, 20 ticks is one second) : Default value is 1200")
+                .define("item_cooldown", 1200);
+        builder.pop();
+
+        // Recipe Randomizer
+        builder.push("Recipe Randomizer");
+        this.randomizeRecipes = builder.comment("Should recipes be randomized per world? Defaults to true")
+                .define("randomize_recipes", true);
+        builder.pop();
+
+        // Structure Randomizer
+        builder.push("Structure Randomizer");
+        this.generateStructures = builder.comment("Should random structures be generated when able? Defaults to true")
+                .define("generate_structures", true);
+        builder.pop();
     }
 
     static {
@@ -34,7 +41,23 @@ public class RandomizerConfig {
                 .configure(RandomizerConfig::new);
     }
 
-    public static int getCooldown(){
-        return RandomizerConfigPair.getLeft().itemCooldown.get();
+    public static RandomizerConfig getConfig(){
+        return RandomizerConfigPair.getLeft();
+    }
+
+    public static int getCooldown() {
+        return getConfig().itemCooldown.get();
+    }
+
+    public static boolean itemRandomizerEnabled() {
+        return getConfig().giveRandomItems.get();
+    }
+
+    public static boolean structureRandomizerEnabled() {
+        return getConfig().generateStructures.get();
+    }
+
+    public static boolean recipeRandomizerEnabled() {
+        return getConfig().randomizeRecipes.get();
     }
 }
