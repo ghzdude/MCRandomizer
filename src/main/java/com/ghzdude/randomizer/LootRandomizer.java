e8c9f57dc5b7a6bc1a50a838c908792e8d7f0d5d
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-05-19 17:53:37 -0700

    update description for LootRandomizer

--- src/main/java/com/ghzdude/randomizer/LootRandomizer.java
+++ src/main/java/com/ghzdude/randomizer/LootRandomizer.java
@@ -1,7 +1,17 @@
 package com.ghzdude.randomizer;
 
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.LootTables;
+
 /* Loot Randomizer
  * Randomizes Loot dropped from mobs, blocks, etc
+ * should only be randomized once per level/world
  */
 public class LootRandomizer {
+
+    public boolean hasChanged = false;
+
+    public void randomizeLootTables(LootTable lootTable) {
+
+    }
 }
