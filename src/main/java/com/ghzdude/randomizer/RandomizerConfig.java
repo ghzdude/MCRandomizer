834a1548163a95dd9e0bfc1a7d30b6466266c868
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-05-22 22:18:27 -0700

    add configs

--- src/main/java/com/ghzdude/randomizer/RandomizerConfig.java
+++ src/main/java/com/ghzdude/randomizer/RandomizerConfig.java
@@ -1,38 +1,65 @@
 package com.ghzdude.randomizer;
 
 import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.config.IConfigSpec;
 import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.function.Predicate;
 
 public class RandomizerConfig {
 
-    public static final Pair<RandomizerConfig, ForgeConfigSpec> RandomizerConfigPair;
-    public final ForgeConfigSpec.ConfigValue<Integer> itemCooldown;
-    public final ForgeConfigSpec.ConfigValue<Boolean> randomizeRecipes;
-    public final ForgeConfigSpec.ConfigValue<Boolean> giveRandomItems;
-    public final ForgeConfigSpec.ConfigValue<Boolean> generateStructures;
-
+    private static final Pair<RandomizerConfig, ForgeConfigSpec> RandomizerConfigPair;
+    private final ForgeConfigSpec.ConfigValue<Integer> itemCooldown;
+    private final ForgeConfigSpec.ConfigValue<Boolean> randomizeRecipes;
+    private final ForgeConfigSpec.ConfigValue<Boolean> giveRandomItems;
+    private final ForgeConfigSpec.ConfigValue<Boolean> generateStructures;
+    private final ForgeConfigSpec.ConfigValue<Integer> structureProbability;
+    private final ForgeConfigSpec.ConfigValue<Boolean> randomizeLoot;
+    private final ForgeConfigSpec.ConfigValue<Boolean> randomizeBloockLoot;
+    private final ForgeConfigSpec.ConfigValue<Boolean> randomizeEntityLoot;
+    private final ForgeConfigSpec.ConfigValue<Boolean> randomizeChestLoot;
 
     RandomizerConfig (ForgeConfigSpec.Builder builder) {
 
         // Item Randomizer
         builder.push("Item Randomizer");
-        this.giveRandomItems = builder.comment("Should random items be given to the player? Defaults to true")
+        this.giveRandomItems = builder.comment("Should random items be given to the player? Defaults to true.")
                 .define("give_random_items", true);
 
-        this.itemCooldown = builder.comment("Time between items given (measured in ticks, 20 ticks is one second) : Default value is 1200")
+        this.itemCooldown = builder.comment("Time between items given (measured in ticks, 20 ticks is one second) : Default value is 1200.")
                 .define("item_cooldown", 1200);
         builder.pop();
 
         // Recipe Randomizer
         builder.push("Recipe Randomizer");
-        this.randomizeRecipes = builder.comment("Should recipes be randomized per world? Defaults to true")
+        this.randomizeRecipes = builder.comment("Should recipes be randomized per world? Defaults to true.")
                 .define("randomize_recipes", true);
         builder.pop();
 
         // Structure Randomizer
         builder.push("Structure Randomizer");
-        this.generateStructures = builder.comment("Should random structures be generated when able? Defaults to true")
+        this.generateStructures = builder.comment("Should random structures be generated when able? Defaults to true.")
                 .define("generate_structures", true);
+
+        Predicate<Object> validateProbability = o -> Integer.parseInt(o.toString()) > 0 || Integer.parseInt(o.toString()) < 100;
+        this.structureProbability = builder.comment("Probability of how likely structure generation is picked over item generation. Defaults to 10, value must be between 0 and 100 ")
+                        .define("structure_probability", 10, validateProbability);
+
+        builder.pop();
+
+        builder.push("Loot Randomizer");
+        this.randomizeLoot = builder.comment("Should Loot Randomizer be enabled at all? Defaults to true.")
+                .define("randomize_loot", true);
+
+        this.randomizeBloockLoot = builder.comment("Should block drops be randomized? Defaults to false.")
+                .define("randomize_block_loot", false);
+
+        this.randomizeEntityLoot = builder.comment("Should mob drops be randomized? Defaults to false.")
+                .define("randomize_entity_loot", false);
+
+        this.randomizeChestLoot = builder.comment("Should chest loot be randomized? Defaults to true.")
+                        .define("randomize_chest_loot", true);
+
         builder.pop();
     }
 
@@ -43,6 +70,10 @@ public class RandomizerConfig {
 
     public static RandomizerConfig getConfig(){
         return RandomizerConfigPair.getLeft();
+    }
+
+    public static IConfigSpec<?> getSpec() {
+        return RandomizerConfigPair.getRight();
     }
 
     public static int getCooldown() {
@@ -57,7 +88,27 @@ public class RandomizerConfig {
         return getConfig().generateStructures.get();
     }
 
+    public static int getStructureProbability() {
+        return getConfig().structureProbability.get();
+    }
+
     public static boolean recipeRandomizerEnabled() {
         return getConfig().randomizeRecipes.get();
+    }
+
+    public static boolean randomizeLoot() {
+        return getConfig().randomizeLoot.get();
+    }
+
+    public static boolean randomizeBlockLoot() {
+        return getConfig().randomizeBloockLoot.get();
+    }
+
+    public static boolean randomizeEntityLoot() {
+        return getConfig().randomizeEntityLoot.get();
+    }
+
+    public static boolean randomizeChestLoot() {
+        return getConfig().randomizeChestLoot.get();
     }
 }
