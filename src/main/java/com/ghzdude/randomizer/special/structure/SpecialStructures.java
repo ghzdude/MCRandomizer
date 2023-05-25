64f7929e299bddbb139aff06e66056a3fb126844
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-05-25 01:45:58 -0700

    increase ancient city value

--- src/main/java/com/ghzdude/randomizer/special/structure/SpecialStructures.java
+++ src/main/java/com/ghzdude/randomizer/special/structure/SpecialStructures.java
@@ -8,7 +8,7 @@ public class SpecialStructures {
 
     public static final SpecialStructureList CONFIGURED_STRUCTURES = new SpecialStructureList(List.of(
             new SpecialStructure(Structures.NETHER_FOSSIL, 0),
-            new SpecialStructure(Structures.ANCIENT_CITY, 2),
+            new SpecialStructure(Structures.ANCIENT_CITY, 6),
             new SpecialStructure(Structures.BASTION_REMNANT, 2),
             new SpecialStructure(Structures.FORTRESS, 2),
             new SpecialStructure(Structures.STRONGHOLD, 3),
