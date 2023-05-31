0a6b416cb1ff3fbcd242ab1698f6616499b8c63a
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-05-30 21:52:06 -0700

    add methods to SpecialItemList

--- src/main/java/com/ghzdude/randomizer/special/item/SpecialItemList.java
+++ src/main/java/com/ghzdude/randomizer/special/item/SpecialItemList.java
@@ -13,21 +13,44 @@ public class SpecialItemList extends ArrayList<SpecialItem> {
     }
     @Override
     public boolean contains(Object o) {
-        if (!(o instanceof SpecialItem)) return false;
+        if (o instanceof SpecialItem) {
+            return contains((SpecialItem) o);
+        } else if (o instanceof Item) {
+            return contains((Item) o);
+        }
+        return false;
+    }
+
+    public boolean contains(SpecialItem o) {
+        return contains(o.item);
+    }
+
+    public boolean contains(Item o) {
+        if (o == null) return false;
         for (SpecialItem item : this) {
-            if (item.item.equals(((SpecialItem) o).item)) return true;
+            if (item.item.equals(o)) return true;
         }
         return false;
     }
 
     @Override
     public int indexOf(Object o) {
+        if (o instanceof SpecialItem) {
+            return indexOf((SpecialItem) o);
+        } else if (o instanceof Item) {
+            return indexOf((Item) o);
+        }
+        return -1;
+    }
+
+    public int indexOf(SpecialItem o) {
+        return indexOf(o.item);
+    }
+
+    public int indexOf(Item o) {
+        if (o == null) return -1;
         for (int i = 0; i < this.size(); i++) {
-            if (o instanceof ItemLike) {
-                if (this.get(i).item == o) return i;
-            } else if (o instanceof SpecialItem) {
-                if (this.get(i).item == ((SpecialItem) o).item) return i;
-            }
+            if (this.get(i).item == o) return i;
         }
         return -1;
     }
