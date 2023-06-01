acd6cf4b4580695056ad3b680fbb8c49b6856adb
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-05-31 21:53:10 -0700

    add more methods to SpecialStructureList

--- src/main/java/com/ghzdude/randomizer/special/structure/SpecialStructureList.java
+++ src/main/java/com/ghzdude/randomizer/special/structure/SpecialStructureList.java
@@ -1,6 +1,7 @@
 package com.ghzdude.randomizer.special.structure;
 
 
+import net.minecraft.world.level.levelgen.structure.Structure;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -18,6 +19,17 @@ public class SpecialStructureList extends ArrayList<SpecialStructure> {
 
     @Override
     public boolean contains(Object o) {
+        if (o instanceof SpecialStructure) {
+            return contains((SpecialStructure) o);
+        } else if (o instanceof Structure) {
+            return contains((Structure) o);
+        }
+        return false;
+    }
+    public boolean contains(SpecialStructure o) {
+        return contains(o.structure);
+    }
+    public boolean contains(Structure o) {
         for (SpecialStructure structure : this) {
             if (structure.structure.equals(o)) return true;
         }
@@ -26,6 +38,19 @@ public class SpecialStructureList extends ArrayList<SpecialStructure> {
 
     @Override
     public int indexOf(Object o) {
+        if (o instanceof SpecialStructure) {
+            return indexOf((SpecialStructure) o);
+        } else if (o instanceof Structure) {
+            return indexOf((Structure) o);
+        }
+        return -1;
+    }
+
+    public int indexOf(SpecialStructure o) {
+        return indexOf(o.structure);
+    }
+
+    public int indexOf(Structure o) {
         for (int i = 0; i < this.size(); i++) {
             if (this.get(i).structure.equals(o)) return i;
         }
