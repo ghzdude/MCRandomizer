b5a3e12beb63f71feb7b9a5a88d2252135da8d2e
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-10-29 13:40:17 -0700

    remove loot condition formatting

--- src/main/java/com/ghzdude/randomizer/datagen/GlobalLootProvider.java
+++ src/main/java/com/ghzdude/randomizer/datagen/GlobalLootProvider.java
@@ -3,8 +3,6 @@ package com.ghzdude.randomizer.datagen;
 import com.ghzdude.randomizer.RandomizerCore;
 import com.ghzdude.randomizer.loot.LootRandomizeModifier;
 import net.minecraft.data.PackOutput;
-import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
-import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
 import net.minecraftforge.common.data.GlobalLootModifierProvider;
 
 public class GlobalLootProvider extends GlobalLootModifierProvider {
@@ -14,8 +12,6 @@ public class GlobalLootProvider extends GlobalLootModifierProvider {
 
     @Override
     protected void start() {
-        add("randomize_loot", new LootRandomizeModifier(new LootItemCondition[]{
-                LootItemRandomChanceCondition.randomChance(1.0f).build()
-        }));
+        add("randomize_loot", new LootRandomizeModifier());
     }
 }
