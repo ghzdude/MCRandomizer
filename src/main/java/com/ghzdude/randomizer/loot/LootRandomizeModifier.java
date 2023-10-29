77c2d7bc4bc4d7a51978c0a75274dfd72ab92f92
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-10-29 13:56:32 -0700

    raname LootData

--- src/main/java/com/ghzdude/randomizer/loot/LootRandomizeModifier.java
+++ src/main/java/com/ghzdude/randomizer/loot/LootRandomizeModifier.java
@@ -32,7 +32,7 @@ public class LootRandomizeModifier extends LootModifier {
     protected @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
         ObjectArrayList<ItemStack> ret = new ObjectArrayList<>();
         String path = context.getQueriedLootTableId().getPath();
-        LootRandomizer.LootData data = LootRandomizer.get(context.getLevel().getServer().overworld().getDataStorage());
+        LootRandomizer.ItemRandomMapData data = LootRandomizer.get(context.getLevel().getServer().overworld().getDataStorage());
         if (!RandomizerConfig.randomizeBlockLoot() && path.contains("blocks/") ||
             !RandomizerConfig.randomizeEntityLoot() && path.contains("entities/") ||
             !RandomizerConfig.randomizeChestLoot() && path.contains("chests/")
