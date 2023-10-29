1926101fa7b4a05102a8f6042a2e9fb5831690bc
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-10-29 14:13:16 -0700

    GoatHornGenerator now works

--- src/main/java/com/ghzdude/randomizer/special/generators/GoatHornGenerator.java
+++ src/main/java/com/ghzdude/randomizer/special/generators/GoatHornGenerator.java
@@ -1,7 +1,7 @@
 package com.ghzdude.randomizer.special.generators;
 
 import com.ghzdude.randomizer.RandomizerCore;
-import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.*;
@@ -9,11 +9,11 @@ import net.minecraft.world.item.*;
 import java.util.ArrayList;
 
 public class GoatHornGenerator {
-//    private static ArrayList<ResourceLocation> instrumentList = new ArrayList<>(Registry.INSTRUMENT.keySet());
-//    public static void applyGoatHornSound(ItemStack stack) {
-//        CompoundTag baseTag = new CompoundTag();
-//        int id = RandomizerCore.RANDOM.nextInt(instrumentList.size());
-//        baseTag.putString("instrument", instrumentList.get(id).toString());
-//        stack.setTag(baseTag);
-//    }
+    private static final ArrayList<ResourceLocation> INSTRUMENT_LIST = new ArrayList<>(BuiltInRegistries.INSTRUMENT.keySet());
+    public static void applyGoatHornSound(ItemStack stack) {
+        CompoundTag baseTag = new CompoundTag();
+        int id = RandomizerCore.rng.nextInt(INSTRUMENT_LIST.size());
+        baseTag.putString("instrument", INSTRUMENT_LIST.get(id).toString());
+        stack.setTag(baseTag);
+    }
 }
