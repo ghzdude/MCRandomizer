9260420ae3e29041f1ea4ef2f39bb688eed2e9b1
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-10-30 18:57:23 -0700

    add TagKeys to ItemRandomMapData

--- src/main/java/com/ghzdude/randomizer/ItemRandomizer.java
+++ src/main/java/com/ghzdude/randomizer/ItemRandomizer.java
@@ -9,6 +9,7 @@ import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.util.datafix.DataFixTypes;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Inventory;
@@ -18,6 +19,7 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.level.saveddata.SavedData;
 import net.minecraft.world.level.storage.DimensionDataStorage;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.tags.ITagManager;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
@@ -156,19 +158,29 @@ public class ItemRandomizer {
 
     public static class ItemRandomMapData extends SavedData {
 
-        public static final Map<Item, Item> ITEM_MAP = new Object2ObjectOpenHashMap<>();
-        public static ItemRandomMapData INSTANCE;
+
+        private final Map<Item, Item> ITEM_MAP = new Object2ObjectOpenHashMap<>();
+        private final Map<TagKey<Item>, TagKey<Item>> TAGKEY_MAP = new Object2ObjectOpenHashMap<>();
+        public static final ItemRandomMapData INSTANCE = new ItemRandomMapData();
 
         public static SavedData.Factory<ItemRandomMapData> factory() {
             return new Factory<>(ItemRandomMapData::new, ItemRandomMapData::load, DataFixTypes.LEVEL);
+        }
+
+        public void configure() {
+            generateItemMap();
+            generateTagMap();
+            setDirty();
         }
 
         @Override
         public @NotNull CompoundTag save(CompoundTag tag) {
             RandomizerCore.LOGGER.warn("Saving changed loot tables to world data!");
             ListTag map = new ListTag();
-            ITEM_MAP.forEach((vanilla, random) -> {
+            this.ITEM_MAP.forEach((vanilla, random) -> {
                 CompoundTag itemPair = new CompoundTag();
+                if (random == Items.AIR) return;
+
                 itemPair.putString(vanilla.toString(), random.toString());
                 map.add(itemPair);
             });
@@ -179,28 +191,51 @@ public class ItemRandomizer {
         public static ItemRandomMapData load(CompoundTag tag) {
             RandomizerCore.LOGGER.warn("Loading changed loot tables to world data!");
             CompoundTag map = tag.getCompound("item_map");
-            ForgeRegistries.ITEMS.forEach((item) -> {
-                Item random = ForgeRegistries.ITEMS.getValue(new ResourceLocation(map.getString(item.toString())));
-                ITEM_MAP.put(item, random);
+            VALID_ITEMS.forEach((specialItem) -> {
+                Item vanilla = specialItem.item;
+                Item random = ForgeRegistries.ITEMS.getValue(new ResourceLocation(map.getString(vanilla.toString())));
+                if (random == Items.AIR) return;
+
+                INSTANCE.ITEM_MAP.put(vanilla, random);
             });
+            INSTANCE.setDirty();
             return INSTANCE;
         }
 
-        public void configure() {
-            List<Item> registry = Lists.newArrayList(ItemRandomizer.VALID_ITEMS.asItems());
+        private void generateTagMap() {
+            ITagManager<Item> tagManager = ForgeRegistries.ITEMS.tags();
+            if (tagManager == null) return;
 
-            List<Item> blocks = Lists.newArrayList(registry);
-            Collections.shuffle(blocks, RandomizerCore.rng);
+            List<TagKey<Item>> vanilla = tagManager.getTagNames().toList();
+            List<TagKey<Item>> randomized = Lists.newArrayList(vanilla);
+            Collections.shuffle(randomized, RandomizerCore.rng);
 
-            if (registry.size() != blocks.size()) {
-                RandomizerCore.LOGGER.warn("Registry was modified during server start!");
+            if (vanilla.size() != randomized.size()) {
+                RandomizerCore.LOGGER.warn("Tagkey registry was modified during server start!");
                 return;
             }
 
-            for (int i = 0; i < registry.size(); i++) {
-                if (ITEM_MAP.containsKey(registry.get(i))) continue;
+            for (int i = 0; i < vanilla.size(); i++) {
+                if (TAGKEY_MAP.containsKey(vanilla.get(i))) continue;
 
-                ITEM_MAP.put(registry.get(i), blocks.get(i));
+                TAGKEY_MAP.put(vanilla.get(i), randomized.get(i));
+            }
+        }
+
+        private void generateItemMap() {
+            List<Item> vanilla = Lists.newArrayList(VALID_ITEMS.asItems());
+            List<Item> randomized = Lists.newArrayList(vanilla);
+            Collections.shuffle(randomized, RandomizerCore.rng);
+
+            if (vanilla.size() != randomized.size()) {
+                RandomizerCore.LOGGER.warn("Item registry was modified during server start!");
+                return;
+            }
+
+            for (int i = 0; i < vanilla.size(); i++) {
+                if (ITEM_MAP.containsKey(vanilla.get(i))) continue;
+
+                ITEM_MAP.put(vanilla.get(i), randomized.get(i));
             }
             setDirty();
         }
