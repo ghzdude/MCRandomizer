40e704d85edc45bf38c4ab06e52f8e25098bf4af
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-11-06 19:50:51 -0700

    remove recipe advancements outright add new recipe advancements in everything works now

--- src/main/java/com/ghzdude/randomizer/RecipeRandomizer.java
+++ src/main/java/com/ghzdude/randomizer/RecipeRandomizer.java
@@ -2,20 +2,27 @@ package com.ghzdude.randomizer;
 
 import com.ghzdude.randomizer.reflection.ReflectionUtils;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementHolder;
 import net.minecraft.advancements.AdvancementRewards;
+import net.minecraft.advancements.critereon.InventoryChangeTrigger;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.ServerAdvancementManager;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.*;
 import net.minecraftforge.event.server.ServerStartedEvent;
 import net.minecraftforge.event.server.ServerStoppingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.registries.tags.ITag;
+import net.minecraftforge.registries.tags.ITagManager;
 
+import javax.annotation.Nonnull;
 import java.util.*;
 
 /* Recipe Randomizer Description.
@@ -33,22 +40,18 @@ import java.util.*;
  *
  */
 public class RecipeRandomizer {
-
-    private static final Map<ResourceLocation, ResourceLocation[]> VANILLA = new Object2ObjectOpenHashMap<>();
-    private static final Map<ResourceLocation, ResourceLocation[]> MODIFIED = new Object2ObjectOpenHashMap<>();
+    private static final Map<ResourceLocation, List<ResourceLocation>> MODIFIED = new Object2ObjectOpenHashMap<>();
 
     @SubscribeEvent
     public void start(ServerStartedEvent event) {
         if (RandomizerConfig.recipeRandomizerEnabled()) {
             RandomizerCore.LOGGER.warn("Recipe Randomizer Running!");
-            Collection<AdvancementHolder> advancementHolders = event.getServer().getAdvancements().getAllAdvancements()
-                    .stream().filter(advancementHolder -> advancementHolder.id().getPath().contains("recipes/"))
-                    .toList();
             randomizeRecipes(
                     event.getServer().getRecipeManager(),
                     event.getServer().registryAccess()
             );
-            tryModifyAdvancements(advancementHolders);
+
+            setAdvancements(event.getServer().getAdvancements());
         }
     }
 
@@ -56,6 +59,18 @@ public class RecipeRandomizer {
     public void stop(ServerStoppingEvent event) {
         MODIFIED.clear();
     }
+
+    public static void setAdvancements(ServerAdvancementManager manager) {
+        Map<ResourceLocation, AdvancementHolder> holders = ReflectionUtils.getField(ServerAdvancementManager.class, manager, 2);
+        Map<ResourceLocation, AdvancementHolder> toKeep = new Object2ObjectOpenHashMap<>();
+        holders.forEach(((resourceLocation, holder) -> {
+            if (!holder.id().getPath().contains("recipes/")) toKeep.put(resourceLocation, holder);
+        }));
+
+        buildAdvancements(toKeep);
+        ReflectionUtils.setField(ServerAdvancementManager.class, manager, 2, toKeep);
+    }
+
     public void randomizeRecipes(RecipeManager manager, RegistryAccess access) {
         for (RecipeHolder<?> holder : manager.getRecipes()) {
             Recipe<?> recipe = holder.value();
@@ -64,11 +79,14 @@ public class RecipeRandomizer {
             modifyRecipeOutputs(recipe, newResult);
 
             // if inputs are not to be randomized, move on to the next recipe
-            if (RandomizerConfig.randomizeInputs()) modifyRecipeInputs(
-                    recipe.getIngredients().stream().distinct().filter(ingredient -> !ingredient.isEmpty()).toList(), holder.id());
+            if (RandomizerConfig.randomizeInputs()) {
+                modifyRecipeInputs(
+                        recipe.getIngredients().stream()
+                        .distinct().filter(ingredient -> !ingredient.isEmpty()).toList(), holder.id()
+                );
+            }
         }
     }
-
 
     private void modifyRecipeOutputs(Recipe<?> recipe, ItemStack newResult) {
         if (recipe instanceof ShapedRecipe shapedRecipe) {
@@ -84,58 +102,67 @@ public class RecipeRandomizer {
 
     private void modifyRecipeInputs(List<Ingredient> ingredients, ResourceLocation recipe) {
         IForgeRegistry<Item> registry = ForgeRegistries.ITEMS;
+
         for (int k = 0; k < ingredients.size(); k++) {
             Ingredient.Value[] values = ReflectionUtils.getField(Ingredient.class, ingredients.get(k), 2);
             if (values.length == 0 || Arrays.stream(values).allMatch(Objects::isNull)) continue;
-            ResourceLocation[] vanilla = new ResourceLocation[values.length];
-            ResourceLocation[] modified = new ResourceLocation[values.length];
+            ResourceLocation ingredient = null;
 
             for (int j = 0; j < values.length; j++) {
                 if (values[j] instanceof Ingredient.ItemValue itemValue) {
-                    vanilla[j] = registry.getKey(itemValue.item().getItem());
                     ItemStack stack = ItemRandomizer.getStackFor(itemValue.item());
+                    ingredient = registry.getKey(stack.getItem());
                     values[j] = new Ingredient.ItemValue(stack);
-                    modified[j] = registry.getKey(stack.getItem());
                 } else if (values[j] instanceof Ingredient.TagValue tagValue) {
                     TagKey<Item> key = ItemRandomizer.getTagKeyFor(tagValue.tag());
-                    vanilla[j] = tagValue.tag().location();
+                    ingredient = key.location();
                     values[j] = new Ingredient.TagValue(key);
-                    modified[j] = key.location();
                 }
+                if (ingredient == null) return;
+
             }
-            addToMap(VANILLA, vanilla, recipe);
-            addToMap(MODIFIED, modified, recipe);
+            addToMap(recipe, ingredient);
         }
     }
 
-    private void addToMap(Map<ResourceLocation, ResourceLocation[]> map, ResourceLocation[] toAdd, ResourceLocation recipe) {
-        if (map.containsKey(recipe)) {
-            if (toAdd.length == 0) {
-                RandomizerCore.LOGGER.warn("{} was given no inputs!", recipe);
-            } else if (map.get(recipe) == null) {
-                map.replace(recipe, toAdd);
-            }
+    public static void addToMap(@Nonnull ResourceLocation recipe, @Nonnull ResourceLocation ingredient) {
+        if (RecipeRandomizer.MODIFIED.containsKey(ingredient)) {
+            RecipeRandomizer.MODIFIED.get(ingredient).add(recipe);
         } else {
-            map.put(recipe, toAdd);
+            List<ResourceLocation> list = new ArrayList<>();
+            list.add(recipe);
+            RecipeRandomizer.MODIFIED.put(ingredient, list);
         }
     }
 
-    private void tryModifyAdvancements(Collection<AdvancementHolder> advancementHolders) {
-        for (AdvancementHolder holder : advancementHolders) {
-            AdvancementRewards rewards = holder.value().rewards();
-            if (rewards.getRecipes().length == 0) continue;
+    private static void buildAdvancements(Map<ResourceLocation, AdvancementHolder> map) {
+        ITagManager<Item> tagManager = ForgeRegistries.ITEMS.tags();
+        if (tagManager == null) return;
 
-            List<ResourceLocation> updated = new ArrayList<>();
-            for (ResourceLocation resourceLocation : rewards.getRecipes()) {
-                if (!VANILLA.containsKey(resourceLocation)) continue;
-                ResourceLocation[] ingredients = VANILLA.get(resourceLocation);
-                MODIFIED.entrySet().stream()
-                    .filter(entry -> entry.getValue().length != ingredients.length)
-                    .filter(entry -> ingredients[0] == entry.getValue()[0])
-                    .forEach(entry -> updated.add(entry.getKey()));
+        MODIFIED.forEach((ing, recipes) -> {
+            Item[] changedItems;
+            Item item = ForgeRegistries.ITEMS.getValue(ing);
+            Optional<ITag<Item>> tag = tagManager.getTagNames()
+                    .filter(key -> key.location().equals(ing))
+                    .map(tagManager::getTag)
+                    .findFirst();
+
+            if (item != Items.AIR) {
+                changedItems = new Item[]{item};
+            } else if (tag.isPresent()) {
+                changedItems = tag.get().stream().toArray(Item[]::new);
+            } else {
+                RandomizerCore.LOGGER.warn("{} is not a valid item or tag!", ing);
+                return;
             }
-            if (updated.size() == 0) updated.add(new ResourceLocation("empty"));
-            ReflectionUtils.setField(AdvancementRewards.class, rewards, 3, updated.toArray(ResourceLocation[]::new));
-        }
+
+            Advancement.Builder builder = new Advancement.Builder();
+            for (ResourceLocation recipe : recipes) {
+                builder.rewards(AdvancementRewards.Builder.recipe(recipe));
+            }
+            builder.addCriterion("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(changedItems));
+            AdvancementHolder toAdd = builder.build(new ResourceLocation(RandomizerCore.MODID, ing.getNamespace() + "-" + ing.getPath() + "_gives_recipes"));
+            map.put(toAdd.id(), toAdd);
+        });
     }
 }
