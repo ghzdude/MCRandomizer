3e95abfeedf8e0cf8325f4c1a48cb6dfe8d009e2
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-11-09 15:46:09 -0700

    make LootRandomizeModifier use it's own map instance

--- src/main/java/com/ghzdude/randomizer/loot/LootRandomizeModifier.java
+++ src/main/java/com/ghzdude/randomizer/loot/LootRandomizeModifier.java
@@ -1,6 +1,6 @@
 package com.ghzdude.randomizer.loot;
 
-import com.ghzdude.randomizer.ItemRandomizer;
+import com.ghzdude.randomizer.RandomizationMapData;
 import com.ghzdude.randomizer.RandomizerConfig;
 import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
@@ -8,6 +8,7 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.storage.DimensionDataStorage;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.loot.IGlobalLootModifier;
@@ -15,6 +16,8 @@ import net.minecraftforge.common.loot.LootModifier;
 import org.jetbrains.annotations.NotNull;
 
 public class LootRandomizeModifier extends LootModifier {
+
+    private static RandomizationMapData INSTANCE = null;
     public static final Supplier<Codec<LootRandomizeModifier>> CODEC = Suppliers.memoize(() ->
             RecordCodecBuilder.create(inst ->
                     codecStart(inst).apply(inst, LootRandomizeModifier::new))
@@ -32,6 +35,11 @@ public class LootRandomizeModifier extends LootModifier {
     protected @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
         if (!RandomizerConfig.lootRandomizerEnabled()) return generatedLoot;
 
+        if (INSTANCE == null) {
+            DimensionDataStorage storage = context.getLevel().getServer().overworld().getDataStorage();
+            INSTANCE = RandomizationMapData.configure(storage, "loot");
+        }
+
         ObjectArrayList<ItemStack> ret = new ObjectArrayList<>();
         String path = context.getQueriedLootTableId().getPath();
         if (!RandomizerConfig.randomizeBlockLoot() && path.contains("blocks/") ||
@@ -43,7 +51,7 @@ public class LootRandomizeModifier extends LootModifier {
 
         generatedLoot.forEach(stack -> {
             if (stack.isEmpty()) return;
-            ret.add(ItemRandomizer.getStackFor(stack));
+            ret.add(INSTANCE.getStackFor(stack));
         });
 
         return ret;
