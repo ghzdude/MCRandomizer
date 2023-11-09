e1a4832cdd0790e2e0109f789c70396ef5acfbf7
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-11-09 15:42:59 -0700

    remove LootRandomizer.java

--- src/main/java/com/ghzdude/randomizer/LootRandomizer.java
+++
@@ -1,9 +0,0 @@
-package com.ghzdude.randomizer;
-
-/* Loot Randomizer
- * Randomizes Loot dropped from mobs, blocks, etc
- * should only be randomized once per level/world
- */
-public class LootRandomizer {
-
-}
