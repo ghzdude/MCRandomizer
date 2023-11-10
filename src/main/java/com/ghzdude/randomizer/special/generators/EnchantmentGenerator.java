3ae4bd9e9b260d10e47a1427f451b9344a9d1b7f
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-11-09 21:19:33 -0700

    fix EnchantmentGenerator increase max enchant level

--- src/main/java/com/ghzdude/randomizer/special/generators/EnchantmentGenerator.java
+++ src/main/java/com/ghzdude/randomizer/special/generators/EnchantmentGenerator.java
@@ -7,7 +7,9 @@ import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class EnchantmentGenerator {
 
@@ -18,11 +20,16 @@ public class EnchantmentGenerator {
         int shouldEnchant = rng.nextInt(0, 100);
         if (shouldEnchant < 80 && !stack.is(Items.ENCHANTED_BOOK)) return;
 
-        int numOfEnchants = rng.nextInt(3) + 1;
+        List<Enchantment> applicable = VALID_ENCHANTS.stream().filter(stack::canApplyAtEnchantingTable).collect(Collectors.toList());
+
+        int numOfEnchants = rng.nextInt(applicable.size()) + 1;
         for (int i = 0; i < numOfEnchants; i++) {
-            int id = rng.nextInt(VALID_ENCHANTS.size());
-            Enchantment toApply = VALID_ENCHANTS.get(id);
-            stack.enchant(toApply, rng.nextInt(toApply.getMaxLevel()) + 1);
+            int id = rng.nextInt(applicable.size());
+            Enchantment toApply = applicable.get(id);
+            applicable.remove(id);
+
+            // todo handle max enchant level not translating properly
+            stack.enchant(toApply, rng.nextInt(15));
         }
     }
 }
