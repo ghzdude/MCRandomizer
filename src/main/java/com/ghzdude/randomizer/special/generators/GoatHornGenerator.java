1dd23001267f4d8d4866f905905776932b9e9638
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-11-12 17:19:14 -0700

    fix goat horn generator

--- src/main/java/com/ghzdude/randomizer/special/generators/GoatHornGenerator.java
+++ src/main/java/com/ghzdude/randomizer/special/generators/GoatHornGenerator.java
@@ -1,15 +1,16 @@
 package com.ghzdude.randomizer.special.generators;
 
 import com.ghzdude.randomizer.RandomizerCore;
-import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.Registry;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.*;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.registries.RegistryManager;
 
 import java.util.ArrayList;
 
 public class GoatHornGenerator {
-    private static final ArrayList<ResourceLocation> INSTRUMENT_LIST = new ArrayList<>(BuiltInRegistries.INSTRUMENT.keySet());
+    private static final ArrayList<ResourceLocation> INSTRUMENT_LIST = new ArrayList<>(RegistryManager.ACTIVE.getRegistry(Registry.INSTRUMENT_REGISTRY).getKeys());
     public static void applyGoatHornSound(ItemStack stack) {
         CompoundTag baseTag = new CompoundTag();
         int id = RandomizerCore.unseededRNG.nextInt(INSTRUMENT_LIST.size());
