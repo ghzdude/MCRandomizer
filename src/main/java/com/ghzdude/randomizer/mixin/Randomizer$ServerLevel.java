0d62a42e69a55b4e24a0394f65f1d68a648f18e8
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-05-13 12:35:28 -0700

    remove unneeded mixins

--- src/main/java/com/ghzdude/randomizer/mixin/Randomizer$ServerLevel.java
+++
@@ -1,8 +0,0 @@
-package com.ghzdude.randomizer.mixin;
-
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.entity.EntityLookup;
-
-public interface Randomizer$ServerLevel {
-    EntityLookup<Entity> randomizer$getLookUp();
-}
