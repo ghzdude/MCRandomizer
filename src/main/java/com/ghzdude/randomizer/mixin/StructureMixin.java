c071ae15359c8999cecd82693a5916f9e86fa0a6
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-05-13 13:39:01 -0700

    initial work on structure mixin

---
+++ src/main/java/com/ghzdude/randomizer/mixin/StructureMixin.java
@@ -0,0 +1,30 @@
+package com.ghzdude.randomizer.mixin;
+
+import com.ghzdude.randomizer.RandomizerCore;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.StructureManager;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
+import net.minecraft.world.level.levelgen.structure.StructureStart;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(StructureStart.class)
+public abstract class StructureMixin {
+
+    @Inject(method = "placeInChunk",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/levelgen/structure/StructurePiece;postProcess(Lnet/minecraft/world/level/WorldGenLevel;Lnet/minecraft/world/level/StructureManager;Lnet/minecraft/world/level/chunk/ChunkGenerator;Lnet/minecraft/util/RandomSource;Lnet/minecraft/world/level/levelgen/structure/BoundingBox;Lnet/minecraft/world/level/ChunkPos;Lnet/minecraft/core/BlockPos;)V",
+                    shift = At.Shift.BEFORE),
+            locals = LocalCapture.PRINT)
+    private void InjectStructureModification(WorldGenLevel p_226851_, StructureManager p_226852_,
+                      ChunkGenerator p_226853_, RandomSource p_226854_,
+                      BoundingBox p_226855_, ChunkPos p_226856_, CallbackInfo ci) {
+        RandomizerCore.LOGGER.warn("injected");
+    }
+}
