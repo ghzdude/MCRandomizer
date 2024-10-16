005afe8956af26bc4d840e9c3cb726c4406f00c6
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-10-16 12:55:01 -0700

    mostly works now mixins galore

--- src/main/java/com/ghzdude/randomizer/api/Loot.java
+++ src/main/java/com/ghzdude/randomizer/api/Loot.java
@@ -1,7 +1,10 @@
 package com.ghzdude.randomizer.api;
 
+import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemConditionType;
@@ -10,10 +13,14 @@ import java.util.function.Predicate;
 
 public final class Loot {
     public interface TableInfo {
-        boolean randomizer$requiresSilkTouch(RegistryAccess access);
+        boolean randomizer$hasSilkTouch(RegistryAccess access);
+        ItemStack randomizer$getSilkDrop(RegistryAccess access);
+        ItemStack randomizer$getDrop(RegistryAccess access);
     }
     public interface PoolInfo {
         boolean randomizer$checkForSilkTouch(Registry<Enchantment> registry);
+        Holder<Item> randomizer$getSilkDrop(Registry<Enchantment> registry);
+        Holder<Item> randomizer$getDrop(Registry<Enchantment> registry);
     }
     public interface EntryInfo {
         boolean randomizer$hasCondition(LootItemConditionType type, Predicate<LootItemCondition> predicate);
--- src/main/java/com/ghzdude/randomizer/compat/jei/BlockDropRecipe.java
+++ src/main/java/com/ghzdude/randomizer/compat/jei/BlockDropRecipe.java
@@ -1,5 +1,6 @@
 package com.ghzdude.randomizer.compat.jei;
 
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 import java.util.ArrayList;
@@ -8,8 +9,9 @@ import java.util.List;
 public record BlockDropRecipe(ItemStack input, ItemStack output, boolean silkTouch) {
     private static final List<BlockDropRecipe> REGISTRY = new ArrayList<>();
 
-    public static void registerRecipe(ItemStack in, ItemStack output, boolean silkTouch) {
-        var recipe = new BlockDropRecipe(in, output, silkTouch);
+    public static void registerRecipe(Item in, ItemStack output, boolean silkTouch) {
+        if (output.isEmpty()) return; // todo log?
+        var recipe = new BlockDropRecipe(new ItemStack(in), output, silkTouch);
         REGISTRY.add(recipe);
     }
 
