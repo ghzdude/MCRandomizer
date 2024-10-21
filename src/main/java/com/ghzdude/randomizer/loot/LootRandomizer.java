52d50936d8d401fc5a9932b4cb6753076a711572
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-10-21 14:04:26 -0700

    remove Loot.java and move into LootRandomizer

--- src/main/java/com/ghzdude/randomizer/loot/LootRandomizer.java
+++ src/main/java/com/ghzdude/randomizer/loot/LootRandomizer.java
@@ -2,19 +2,27 @@ package com.ghzdude.randomizer.loot;
 
 import com.ghzdude.randomizer.RandomizationMapData;
 import com.ghzdude.randomizer.RandomizerConfig;
-import com.ghzdude.randomizer.api.Loot;
 import com.ghzdude.randomizer.compat.jei.BlockDropRecipe;
 import com.ghzdude.randomizer.util.RandomizerUtil;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import it.unimi.dsi.fastutil.objects.Reference2ObjectArrayMap;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParam;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
 
 public class LootRandomizer {
 
@@ -23,30 +31,35 @@ public class LootRandomizer {
     public static void init(MinecraftServer server) {
         INSTANCE = RandomizationMapData.get(server, "loot");
 
+        final var hand = createLootParams(server, Items.AIR, false);
+        final var withPick = createLootParams(server, Items.NETHERITE_PICKAXE, false);
+        final var withSilkPick = createLootParams(server, Items.NETHERITE_PICKAXE, true);
+        final var withShears = createLootParams(server, Items.SHEARS, false);
+
         for (Item item : INSTANCE.getItems()) {
             if (!(item instanceof BlockItem blockItem)) continue;
             var table = server.reloadableRegistries().getLootTable(blockItem.getBlock().getLootTable());
+            if (table == LootTable.EMPTY) continue;
 
-            final var hand = Loot.createLootParams(server, blockItem, Items.AIR, false);
-            final var withPick = Loot.createLootParams(server, blockItem, Items.NETHERITE_PICKAXE, false);
-            final var withSilkPick = Loot.createLootParams(server, blockItem, Items.NETHERITE_PICKAXE, true);
-            final var withShears = Loot.createLootParams(server, blockItem, Items.SHEARS, false);
+            hand.updateState(blockItem);
+            withPick.updateState(blockItem);
+            withSilkPick.updateState(blockItem);
+            withShears.updateState(blockItem);
 
-            ItemStack normalDrop = getDrop(table, hand);
+            ItemStack handDrop = getDrop(table, hand);
             ItemStack pickDrop = getDrop(table, withPick);
-
             ItemStack silkDrop = getDrop(table, withSilkPick);
             ItemStack shearDrop = getDrop(table, withShears);
 
-            BlockDropRecipe.registerRecipe(blockItem, INSTANCE.getStackFor(normalDrop));
+            BlockDropRecipe.registerRecipe(blockItem, INSTANCE.getStackFor(handDrop));
 
-            if (!normalDrop.isEmpty() && !ItemStack.isSameItemSameComponents(normalDrop, silkDrop))
+            if (!handDrop.isEmpty() && !ItemStack.isSameItemSameComponents(handDrop, silkDrop))
                 BlockDropRecipe.registerRecipe(blockItem, INSTANCE.getStackFor(silkDrop), BlockDropRecipe.Type.SILK_PICK);
 
-            if (!ItemStack.isSameItemSameComponents(pickDrop, normalDrop))
+            if (!ItemStack.isSameItemSameComponents(pickDrop, handDrop))
                 BlockDropRecipe.registerRecipe(blockItem, INSTANCE.getStackFor(pickDrop), BlockDropRecipe.Type.PICK);
 
-            if (!ItemStack.isSameItemSameComponents(shearDrop, normalDrop)) {
+            if (!ItemStack.isSameItemSameComponents(shearDrop, handDrop)) {
                 BlockDropRecipe.Type type;
                 if (ItemStack.isSameItemSameComponents(shearDrop, silkDrop))
                     type = BlockDropRecipe.Type.SHEARS_OR_SILK;
@@ -82,5 +95,35 @@ public class LootRandomizer {
         }
 
         return ret;
+    }
+
+    private static MutableLootParams createLootParams(ServerLevel level, @NotNull Item tool, boolean silk) {
+        var stack = new ItemStack(tool);
+        if (silk && !stack.isEmpty()) {
+            stack.enchant(level.registryAccess().registryOrThrow(Registries.ENCHANTMENT).getHolderOrThrow(Enchantments.SILK_TOUCH), 1);
+        }
+
+        Map<LootContextParam<?>, Object> lootContext = new Reference2ObjectArrayMap<>();
+        lootContext.put(LootContextParams.TOOL, stack);
+
+        return new MutableLootParams(level, lootContext, Map.of(), 1f);
+    }
+
+    private static MutableLootParams createLootParams(MinecraftServer server, @NotNull Item tool, boolean silk) {
+        return createLootParams(server.overworld(), tool, silk);
+    }
+
+    private static class MutableLootParams extends LootParams {
+
+        private final Map<LootContextParam<?>, Object> params;
+
+        public MutableLootParams(ServerLevel pLevel, Map<LootContextParam<?>, Object> pParams, Map<ResourceLocation, DynamicDrop> pDynamicDrops, float pLuck) {
+            super(pLevel, pParams, pDynamicDrops, pLuck);
+            this.params = pParams;
+        }
+
+        public void updateState(BlockItem item) {
+            params.put(LootContextParams.BLOCK_STATE, item.getBlock().defaultBlockState());
+        }
     }
 }
