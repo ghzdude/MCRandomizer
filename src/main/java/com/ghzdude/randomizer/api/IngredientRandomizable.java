c37f0eaf0a1341cff4da6b0a81aec020565b9586
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2025-10-04 22:22:20 -0700

    comment out IngredientMixin

--- src/main/java/com/ghzdude/randomizer/api/IngredientRandomizable.java
+++ src/main/java/com/ghzdude/randomizer/api/IngredientRandomizable.java
@@ -1,10 +1,6 @@
 package com.ghzdude.randomizer.api;
 
-import net.minecraft.world.item.crafting.Ingredient;
-
-import java.util.function.Function;
-
 public interface IngredientRandomizable {
 
-    void randomizer$randomizeInputs(Function<Ingredient.Value, Ingredient.Value> randomize);
+//    void randomizer$randomizeInputs(Function<Ingredient.Value, Ingredient.Value> randomize);
 }
--- src/main/java/com/ghzdude/randomizer/mixin/IngredientMixin.java
+++ src/main/java/com/ghzdude/randomizer/mixin/IngredientMixin.java
@@ -1,22 +1,15 @@
 package com.ghzdude.randomizer.mixin;
 
 import com.ghzdude.randomizer.api.IngredientRandomizable;
-import net.minecraft.world.item.crafting.Ingredient;
-import org.apache.commons.lang3.ArrayUtils;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 
-import java.util.function.Function;
-
-@Mixin(Ingredient.class)
+//@Mixin(Ingredient.class)
 public abstract class IngredientMixin implements IngredientRandomizable {
 
-    @Shadow @Final
-    private Ingredient.Value[] values;
-
-    @Override
-    public void randomizer$randomizeInputs(Function<Ingredient.Value, Ingredient.Value> randomize) {
-        ArrayUtils.setAll(this.values, i -> randomize.apply(this.values[i]));
-    }
+//    @Shadow @Final
+//    private Ingredient.Value[] values;
+//
+//    @Override
+//    public void randomizer$randomizeInputs(Function<Ingredient.Value, Ingredient.Value> randomize) {
+//        ArrayUtils.setAll(this.values, i -> randomize.apply(this.values[i]));
+//    }
 }
