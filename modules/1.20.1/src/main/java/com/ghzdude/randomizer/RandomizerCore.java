fe64f7709c3056b15b00f1185471a06fa0db6b03
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2026-02-22 08:54:25 -0700

    commit work

--- modules/1.20.1/src/main/java/com/ghzdude/randomizer/RandomizerCore.java
+++ modules/1.20.1/src/main/java/com/ghzdude/randomizer/RandomizerCore.java
@@ -1,4 +1,4 @@
-package com.ghzdude.untitled;
+package com.ghzdude.randomizer;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -30,8 +30,8 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(Untitled.MODID)
-public class Untitled {
+@Mod(RandomizerCore.MODID)
+public class RandomizerCore {
 
     // Define mod id in a common place for everything to reference
     public static final String MODID = "untitled";
@@ -61,7 +61,7 @@ public class Untitled {
             output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public Untitled() {
+    public RandomizerCore() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         // Register the commonSetup method for modloading
